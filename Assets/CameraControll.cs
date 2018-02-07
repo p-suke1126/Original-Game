85b1e361d033eeb1c75ac7d5c3cfab0cd8f9a2e1
megumi sugatani <ririaaa1126@gmail.com>
2018-02-07 20:14:01 +0900

    キャラクターの落下とカメラの追従を追加しました。

---
+++ Assets/CameraControll.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControll : MonoBehaviour
+{
+    //Usagiのオブジェクト
+    private GameObject usagi;
+    //Usagiとカメラの距離
+    private Vector3 difference;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Usagiのオブジェクトを取得
+        this.usagi = GameObject.Find("usagi2");
+        //Usagiとカメラの位置（z座標）の差を求める
+        this.difference = this.transform.position - usagi.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Usagiの位置に合わせてカメラの位置を移動
+        this.transform.position = usagi.transform.position + difference;
+    }
+}
---
+++ Assets/UsagiControll.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UsagiControll : MonoBehaviour
+{
+    //アニメーションするためのコンポーネントを入れる
+    Animator animator;
+
+    //Usagiを移動させるコンポーネントを入れる
+    Rigidbody2D rigid2D;
+
+    // 落下速度の減衰
+   // private float dump = 0.1f;
+
+    // 落下速度
+    float Velocity = -7;
+
+    // Use this for initialization
+    void Start()
+    {
+        // アニメータのコンポーネントを取得する
+        this.animator = GetComponent<Animator>();
+        // Rigidbody2Dのコンポーネントを取得する
+        this.rigid2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // クリックされた場合
+        if (Input.GetMouseButton(0))
+        {
+            // 下方向の力をかける
+            this.rigid2D.velocity = new Vector2(0, this.Velocity);
+        }
+
+        // クリックをやめたら下方向への速度を減速する
+/*        if (Input.GetMouseButton(0) == false)
+        {
+            if (this.rigid2D.velocity.y < 0)
+            {
+                this.rigid2D.velocity *= this.dump;
+            }
+            this.rigid2D.velocity = new Vector2(0,0);
+        }*/
+    }
+}
